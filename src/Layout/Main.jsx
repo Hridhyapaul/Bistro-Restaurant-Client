ba412f8991ac26742f0b53f1f0f2bc522527f749
Your Name <hridhyapaul77@gmail.com>
2023-05-24 16:07:25 +0600

    Added navigation, banner & footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
+import Navigation from "../Shared/Navigation/Navigation";
 
 const Main = () => {
     return (
         <div>
+            <Navigation></Navigation>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
