c1fb5852426df8cd827381cd8e4fb5bc1ccc8e57
Your Name <hridhyapaul77@gmail.com>
2023-05-25 23:35:09 +0600

    Implement Cover With With Blur Parallax Using React Parallax

--- src/Shared/Cover/Cover.jsx
+++ src/Shared/Cover/Cover.jsx
@@ -1,15 +1,25 @@
+import { Parallax } from 'react-parallax';
 
-const Cover = ({img, title, subTitle}) => {
+const Cover = ({ img, title, subTitle }) => {
+
     return (
-        <div className="hero h-[300px] lg:h-[600px]" style={{ backgroundImage: `url("${img}")` }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-xl">
-                    <h1 className="mb-5 text-4xl lg:text-6xl font-bold">{title}</h1>
-                    <p className="mb-5 text-xl lg:text-3xl">{subTitle}</p>
+        <Parallax
+            blur={{ min: -50, max: 50 }}
+            bgImage={img}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            <div className="hero h-[300px] lg:h-[600px]">
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-xl">
+                        <h1 className="mb-5 text-4xl lg:text-6xl font-bold">{title}</h1>
+                        <p className="mb-5 text-xl lg:text-3xl">{subTitle}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax>
+
     );
 };
 
