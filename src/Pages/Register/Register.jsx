6b0f4afe2d0f457463082aa10b0dffa53ed07467
Your Name <hridhyapaul77@gmail.com>
2023-05-27 21:49:31 +0600

    Setuo register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,67 +1,67 @@
-// import { useContext } from "react";
-// import { Link } from "react-router-dom";
-// import { AuthContext } from "../../providers/AuthProvider";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
-// const Register = () => {
-//     const { createUser } = useContext(AuthContext)
+const Register = () => {
+    const { createUser } = useContext(AuthContext)
 
-//     const handleRegister = (event) => {
-//         event.preventDefault();
-//         const form = event.target;
-//         const name = form.name.value;
-//         const email = form.email.value;
-//         const password = form.password.value;
-//         console.log(name, email, password)
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password)
 
-//         createUser(email, password)
-//         .then(result => {
-//             const registeredUser = result.user;
-//             console.log(registeredUser)
-//             form.reset()
-//         })
-//         .catch(error => {
-//             console.log(error.message)
-//         })
-//     }
+        createUser(email, password)
+            .then(result => {
+                const registeredUser = result.user;
+                console.log(registeredUser)
+                form.reset()
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
 
-//     return (
-//         <div className="hero min-h-screen">
-//             <div className="hero-content flex-col lg:flex-row">
-//                 <div className="W-[98%] px-4 text-center lg:text-left">
-//                     <h1 className="text-5xl font-bold">Login now!</h1>
-//                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-//                 </div>
-//                 <div className="card w-[98%] bg-base-100">
-//                     <form className="card-body" onSubmit={handleRegister}>
-//                         <div className="form-control">
-//                             <label className="label">
-//                                 <span className="label-text text-md font-semibold">Name</span>
-//                             </label>
-//                             <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
-//                         </div>
-//                         <div className="form-control">
-//                             <label className="label">
-//                                 <span className="label-text text-md font-semibold">Email</span>
-//                             </label>
-//                             <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
-//                         </div>
-//                         <div className="form-control">
-//                             <label className="label">
-//                                 <span className="label-text text-md font-semibold">Password</span>
-//                             </label>
-//                             <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
-//                         </div>
-//                         <div className="form-control mt-6">
-//                             <input className="btn btn-primary" type="submit" value="Register" />
-//                         </div>
-//                     </form>
-//                     <div className='px-8'>
-//                         <p className='text-center'>Already have an Account? <Link to='/login' className='text-blue-600'>Login</Link> </p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+    return (
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="W-[98%] px-4 text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card w-[98%] bg-base-100">
+                    <form className="card-body" onSubmit={handleRegister}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-md font-semibold">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-md font-semibold">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-md font-semibold">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Register" />
+                        </div>
+                    </form>
+                    <div className='px-8'>
+                        <p className='text-center'>Already have an Account? <Link to='/login' className='text-blue-600'>Login</Link> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// export default Register;
+export default Register;
